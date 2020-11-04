765a2143fb37a3ec667a877f0eb293c9ffd5a650
Igor Stanishovski <igor.stanisovski@gmail.com>
2020-11-04 13:58:16 +0100

    feature/local-storage-for-old-data

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -9,16 +9,7 @@ import { LocationService } from 'src/app/services/location.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  cityName: string;
-  date: Date;
-  currentTemperature: number;
-  maxTemperature: number;
-  minTemperature: number;
-  feelsLikeTemperature: number;
-  currentHumidity: number;
-  currentPressure: number;
-  dataIsAvailable: boolean = false;
-  weatherIcon: string;
+  currentWeather: any = {};
   lon: number;
   lat: number;
   forecast: any[] = [];
@@ -26,6 +17,7 @@ export class HomeComponent implements OnInit {
   constructor(private weatherService: WeatherService, private route:ActivatedRoute, private router:Router, private locationService:LocationService) { }
 
   ngOnInit(): void {
+    this.currentWeather = JSON.parse(localStorage.getItem('currentWeather'));
     this.getCurrentWeatherByName();
     this.getLocation();
   }
@@ -34,15 +26,18 @@ export class HomeComponent implements OnInit {
     await this.weatherService.getCurrentWeather("maribor").subscribe(
       res => { 
         console.log(res);
-        this.cityName = res['name'];
-        this.date = new Date;
-        this.currentTemperature = Math.floor(res['main']['temp'] - 273.15);
-        this.maxTemperature = Math.floor(res['main']['temp_max'] - 273.15);
-        this.minTemperature = Math.floor(res['main']['temp_min'] - 273.15);
-        this.feelsLikeTemperature = Math.floor(res['main']['feels_like'] - 273.15);
-        this.currentHumidity = res['main']['humidity'];
-        this.currentPressure = res['main']['pressure'];
-        this.weatherIcon = res['weather'][0]['icon'];
+        this.currentWeather = {
+          "city": res['name'],
+          "date": new Date,
+          "temperature": Math.floor(res['main']['temp'] - 273.15),
+          "maxTemperature": Math.floor(res['main']['temp_max'] - 273.15),
+          "minTemperature": Math.floor(res['main']['temp_min'] - 273.15),
+          "feelsLike": Math.floor(res['main']['feels_like'] - 273.15),
+          "humidity": res['main']['humidity'],
+          "pressure": res['main']['pressure'],
+          "icon": res['weather'][0]['icon']
+        };
+        localStorage.setItem('currentWeather',JSON.stringify(this.currentWeather));
       },
       err => { console.log(err) }
     )
@@ -54,7 +49,8 @@ export class HomeComponent implements OnInit {
         for(var i = 0;i < 5;i++){
           this.forecast.push(res['daily'][i]);
         }
-        this.dataIsAvailable = true;
+        //this.dataIsAvailable = true;
+        localStorage.setItem('forecast',JSON.stringify(this.forecast));
       },
       err => { console.log(err) }
     )
@@ -69,7 +65,6 @@ export class HomeComponent implements OnInit {
   }
 
   async Refresh() {
-    this.dataIsAvailable = false;
     await this.getCurrentWeatherByName();
     this.forecast = [];
     await this.getWeatherByLocation(this.lon,this.lat);
