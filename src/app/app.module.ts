3cd763162dcf492a1613f26c436022aa76d0fca0
Igor Stanishovski <igor.stanisovski@gmail.com>
2020-11-06 17:43:25 +0100

    feature/updated-i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localeSl from '@angular/common/locales/sl';
+registerLocaleData(localeSl, 'sl');
 
 @NgModule({
   declarations: [
